005d2357a78c08f23096c2c8693cbc27bc3b35be
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2022-02-24 18:52:04 +0900

    ・UsersSeeder作成

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class)->create([
+            'name' => 'ryookazaki',
+            'email' => 'ryo.okazaki@asia-quest.jp',
+        ]);
+
+        factory(User::class, 5)->create();
+    }
+}
