23b8f11b0fdc526598d2f7252e85f4d52f097a00
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2022-03-02 10:42:28 +0900

    migration, factoryを修正

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->dateTime('birthday');
+            $table->string('avatar')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
